ce6ce0c11dca133160256bf7ca33128b15fc4158
PEDRO270207 <135146060+PEDRO270207@users.noreply.github.com>
2024-05-29 09:35:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var cxPrincipal = document.querySelector("cx-Principal");
+var cxPerguntas = document.querySelector("cxPerguntas");
+var cxAlternativas = document.querySelector("cxAlternativas");
+var cxResultado = document.querySelector("cxResultado");
+var txResultado = document.querySelector("txResultado");
+var perguntas = [
+    {
+        enunciado = "A IA pode automatizar tarefas repetitivas.",
+        alternativas = [
+            "Vai eliminar tarefas monotonas",
+            "Vai tirar muitos empregos"
+        ]
+    },
+    {
+        enunciado = "A IA pode tomar decisões baseadas em dados.",
+        alternativas = [
+            "Maravilhoso",
+            "Assustador"
+        ]
+    },
+    {
+        enunciado = "A IA pode substituir certos empregos.",
+        alternativas = [
+            "Não vejo problemas",
+            "É o fim dos empregos como conhecemos hoje"
+        ]
+    },
+    {
+        enunciado = "A IA pode ajudar a prever tendências de mercado.",
+        alternativas = [
+            "Incrível",
+            "Sinistro"
+        ]
+    },
+    {
+        enunciado = "A IA pode trabalhar 24/7 sem pausas.",
+        alternativas = [
+            "Surpreendente",
+            "Mão de obra barata"
+        ]
+    },
+]
